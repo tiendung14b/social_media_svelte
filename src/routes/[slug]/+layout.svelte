9cba48f498de5d5fef63ed95ec55f0ac9f8b5ab1
Tien Dung <dung14b@gmail.com>
2024-05-31 15:46:42 +0700

    for safety pheww

---
+++ src/routes/[slug]/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	import { logout } from '$lib/api/auth.js';
+	import { localUser } from '$lib/stores/localUser.js';
+	import { goto } from '$app/navigation';
+	import { onDestroy } from 'svelte';
+	import '../../app.css';
+	import Layout from '$lib/components/Layout/Layout.svelte';
+
+	onMount(() => {
+		if (!user) {
+			goto('/login');
+		}
+	});
+</script>
+
+<Layout noSidebar={true}>
+	<slot />
+</Layout>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,22 +1,39 @@
 <script>
-	import { onMount } from 'svelte';
-	import { logout } from '$lib/api/auth.js';
 	import { localUser } from '$lib/stores/localUser.js';
-	import { goto } from '$app/navigation';
 	import { onDestroy } from 'svelte';
-	import '../../app.css';
-	import Layout from '$lib/components/Layout/Layout.svelte';
 
 	let user;
 	const unsub = localUser.subscribe((value) => {
 		user = value;
 	});
 
-	onMount(() => {
-		if (!user) {
-			goto('/login');
-		}
+	onDestroy(() => {
+		unsub();
 	});
 </script>
 
-<Layout noSidebar={true}>asjdflaskdfj</Layout>
+<div class="mx-auto w-4/5 overflow-hidden">
+	<div>
+		<div class="rounded-b-md overflow-hidden">
+			<img src={user.coverImageUrl} alt="" class="object-cover h-[280px] w-full" />
+		</div>
+	</div>
+	<div>
+		<div class="flex items-center -mt-5">
+			<div class="flex gap-4 items-center translate-x-10">
+				<img
+					src={user.profileImageUrl}
+					alt=""
+					class="size-[120px] rounded-full border-[3px] border-white box-content"
+				/>
+				<div class="flex flex-col gap-1">
+					<strong class="text-[32px] font-bold">{user.first_name + ' ' + user.last_name}</strong>
+					<span class="text-gray-600">{'@' + user.username}</span>
+				</div>
+			</div>
+			<a class="px-4 py-2 bg-slate-300 rounded-full ml-auto" href="/me">
+				<img src="/images/settings.svg" alt="" class="size-4" />
+			</a>
+		</div>
+	</div>
+</div>
